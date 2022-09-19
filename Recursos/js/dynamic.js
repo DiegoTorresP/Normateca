b97090e09ba74350014e8bebcdc13113d97a38a5
Diego Torres Pérez <87044529+DiegoTorresP@users.noreply.github.com>
2022-09-19 18:58:11 -0500

    cambios dynamic

--- Recursos/js/dynamic.js
+++ Recursos/js/dynamic.js
@@ -42,7 +42,7 @@ function addDarkmodeWidget() {
     //Inicializa contenedor leyes
     $("#ley_1").addClass("active-opciones")
     //Contenido de Imagen
-    $("#leyes_imagen").html("1");
+    $("#leyes_imagen").html("<img src='Recursos/Imagenes/compliant.png' alt='ProtecciónDatos' width='30' height='24' class='' />");
     //Contenido de texto
     $("#leyes_content").html("1");
     //Inicializa contenedor estandares
