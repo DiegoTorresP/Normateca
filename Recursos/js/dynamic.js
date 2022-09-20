a30df5a869820b6e2d8a482bc4147e166039396b
Diego Torres Pérez <87044529+DiegoTorresP@users.noreply.github.com>
2022-09-20 00:01:14 -0500

    Update dynamic.js

--- Recursos/js/dynamic.js
+++ Recursos/js/dynamic.js
@@ -42,7 +42,7 @@ function addDarkmodeWidget() {
     //Inicializa contenedor leyes
     $("#ley_1").addClass("active-opciones")
     //Contenido de Imagen
-    $("#leyes_imagen").html("<img src='https://0.academia-photos.com/attachment_thumbnails/38219766/mini_magick20190226-12499-s4mjri.png?1551169058' alt='ProtecciónDatos' width='300' height='338' class='' />");
+    $("#leyes_imagen").html("<img src='https://0.academia-photos.com/attachment_thumbnails/38219766/mini_magick20190226-12499-s4mjri.png?1551169058' alt='ProtecciónDatos' style='max-width:300px ; min-width: ; max-height:338px ; min-height: ;' class='' />");
     //Contenido de texto
     $("#leyes_content").html("1");
     //Inicializa contenedor estandares
